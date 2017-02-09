33bf56d8fef86fdd2e7de05a680e8d74a6ba29d8
Loc Tran <loctran@Locs-MacBook-Pro.local>
2017-02-09 10:26:16 +0700

    first commit

--- zing/ViewController.swift
+++ zing/ViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image_logo: UIImageView!
+    @IBOutlet weak var label_text: UILabel!
+    @IBOutlet weak var label_name: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIView.animate(withDuration: 4, animations: {
+            self.image_logo!.alpha = 1}) {(finished) in
+                UIView.animate(withDuration: 3, animations: {
+                    self.label_text!.center = CGPoint(x: self.image_logo!.center.x,y: 100)
+                    self.label_text!.alpha = 1
+                }) {(finished) in UIView.animate(withDuration: 3, animations: {self.label_name!.center = CGPoint(x: self.image_logo!.center.x, y:600)
+                    self.label_name!.alpha = 1
+                })
+            }
+        }
     }
 
 
